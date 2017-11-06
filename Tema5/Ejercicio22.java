f9f90f30281071d1bd7b830a5b931bb901985d09
Adrian Sanchez Ramirez <adrisanchez39@gmail.com>
2017-11-06 17:41:46 +0100

    Añadiendo tema 5

---
+++ Tema5/Ejercicio22.java
@@ -0,0 +1,31 @@
+/**
+ * Ejercicio 22
+ * 
+ * @author Adrian Sanchez
+ */
+public class Ejercicio22 {
+
+  public static void main(String[] args) {
+
+    System.out.print("Números primos entre 2 y 100: ");
+
+    boolean esPrimo = true;
+
+    for (int n = 2; n <= 100; n++) {
+
+      // comprueba si n es primo
+      esPrimo = true;
+      for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+          esPrimo = false;
+        }
+      }
+
+      // si n es primo, se muestra por pantalla
+      if (esPrimo) {
+        System.out.print(n + " ");
+      }
+    }
+    System.out.println();
+  }
+}
